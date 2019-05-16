8065e39653ac85647a2d8d1f4acf6e2fbb862b17
Jason Ward <jason.ward@policystat.com>
2019-05-16 13:39:57 -0400

    Bumped to version 0.1.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def get_requirements(path):
 
 setup(
     name="html-tree-diff",
-    version="0.1.3",
+    version="0.1.4",
     description="Structure-aware diff for html and xml documents",
     author="Christian Oudard",
     author_email="christian.oudard@gmail.com",
